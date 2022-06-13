6f4d9a338a38c31e839b0b33479a6ad1c840dd07
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2022-06-13 09:04:26 +0900

    コンポーネント分割

---
+++ src/components/InCompletedTodos.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const InCompletedTodos = (props) => {
+  const { todos, onClickCompleted, onClickDelete } = props;
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickCompleted(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
